c01fb5cac65117dd38d0b05d4f98f978c03b014d
Darien Hernandez <dh@hellofresh.com>
2019-05-05 17:52:11 +0200

    Removed log inside the use case in favor of return the result details.

--- internal/domain/usecase.go
+++ internal/domain/usecase.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"io"
 
-	"github.com/dohernandez/geolocation-service/pkg/log"
 	"github.com/gocarina/gocsv"
 	"github.com/google/uuid"
 )
@@ -31,21 +30,10 @@ func NewImportGeolocationFromCSVFileToDBUseCase(persister Persister) ImportGeolo
 
 // Do executes the usecase logic.
 //
-// Returns error if parser csv file fails.
-//
-// When log is provide thro ctx, execution details are logged.
-//
-// Example of usage:
-//
-//		// ... file *os.File variable is initialized
-//
-//		uc := domain.NewImportGeolocationFromCSVFileToDBUseCase()
-//
-//		// setting log into context to allow to log the execution details
-// 		l := logrus.New()
-// 		ctx := log.ToContext(context.TODO(), l)
-//
-//		uc.Do(ctx, file)
+// Returns error if parser csv file fails otherwise:
+// 		processed - hold the amount of Geolocation processed
+//		accepted - hold the amount of Geolocation inserted into the db
+//		discarded - hold the amount of Geolocation discarded due to invalidation or duplication
 //
 func (uc *importGeolocationFromCSVFileToDBUseCase) Do(ctx context.Context, f io.Reader) (processed, accepted, discarded int, err error) {
 	var gs []Geolocation
@@ -73,63 +61,6 @@ func (uc *importGeolocationFromCSVFileToDBUseCase) Do(ctx context.Context, f io.
 		}
 
 		accepted++
-	}
-
-	//// getting csv header
-	//r := csv.NewReader(f)
-	//line, err := r.Read()
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//var processed, accepted, discarded int
-	//
-	//header := strings.Join(line, ",")
-	//
-	//// parsing csv lines.
-	//// The reason why the file is parser line by line and not all at once is because for those lines where
-	//// the value is not valid type an error is return, therefore the function fails without parse any line.
-	//for {
-	//	line, err := r.Read()
-	//	if err == io.EOF {
-	//		break
-	//	}
-	//	if err != nil {
-	//		return err
-	//	}
-	//
-	//	processed++
-	//
-	//	var gs []Geolocation
-	//
-	//	// concatenate header with line to allow unmarshall into Geolocation struct
-	//	s := header + "\n" + strings.Join(line, ",")
-	//
-	//	// creating inline file
-	//	lr := strings.NewReader(s)
-	//	if err := gocsv.Unmarshal(lr, &gs); err != nil {
-	//		discarded++
-	//
-	//		continue
-	//	}
-	//
-	//	g := gs[0]
-	//	if err := g.Validate(); err != nil {
-	//		discarded++
-	//
-	//		continue
-	//	}
-	//
-	//	accepted++
-	//}
-
-	l := log.FromContext(ctx)
-	if l != nil {
-		l.
-			WithField("processed", processed).
-			WithField("accepted", accepted).
-			WithField("discarded", discarded).
-			Infof("Import Geolocation From CSV to DB")
 	}
 
 	return processed, accepted, discarded, nil
